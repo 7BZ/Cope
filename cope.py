32b1a3b46cff285f6fc6892ca191b63072bdafbf
7BZ <81964245+7BZ@users.noreply.github.com>
2021-04-05 22:23:14 -0400

    Add files via upload

---
+++ cope.py
@@ -0,0 +1,131 @@
+import discord, requests, os, asyncio, colorama, time, asyncio, threading, sys
+from colorama import Fore
+from discord.ext import commands
+
+os.system('cls & mode 50,20 & title Cope - Config')
+token = input(f"[{Fore.RED}Cope{Fore.RESET}]~ Token{Fore.RED}: {Fore.RESET}")
+
+client = commands.Bot(command_prefix=">",self_bot=True,intents=discord.Intents.all())
+client.remove_command('help')
+
+class Cope:
+	def __init__(self):
+		pass
+
+	async def ChannelCopy(self):
+			urguild = input(f"[{Fore.RED}Cope{Fore.RESET}]~ Your Guild ID{Fore.RED}: {Fore.RESET}")
+			guild = input(f"[{Fore.RED}Cope{Fore.RESET}]~ Guild ID{Fore.RED}: {Fore.RESET}")
+			guildOBJ = client.get_guild(int(guild))
+			guildOBJ2 = client.get_guild(int(urguild))
+			for cate in guildOBJ.categories:
+				x = await guildOBJ2.create_category(cate.name)
+				for chann in cate.channels:
+					if isinstance(chann, discord.VoiceChannel):
+						await x.create_voice_channel(chann.name)
+					if isinstance(chann, discord.TextChannel):
+						await x.create_text_channel(chann.name)
+					if isinstance(chann, discord.StageChannel):
+						await x.create_stage_channel(chann.name)
+
+
+	async def DeleteChannels(self):
+			urguild = input(f"[{Fore.RED}Cope{Fore.RESET}]~ Your Guild ID{Fore.RED}: {Fore.RESET}")
+			guildOBJ2 = client.get_guild(int(urguild))
+			for c in guildOBJ2.channels:
+				await c.delete()
+		
+
+	async def RolesCopy(self):
+		urguild = input(f"[{Fore.RED}Cope{Fore.RESET}]~ Your Guild ID{Fore.RED}: {Fore.RESET}")
+		guild = input(f"[{Fore.RED}Cope{Fore.RESET}]~ Guild ID{Fore.RED}: {Fore.RESET}")
+		guildOBJ = client.get_guild(int(guild))
+		guildOBJ2 = client.get_guild(int(urguild))
+		roles = guildOBJ.roles
+		for x in range(len(roles)):
+			if roles[-x].name == guildOBJ2.default_role.name:
+				await guildOBJ2.default_role.edit(permissions=roles[x].permissions)
+			else:
+				await guildOBJ2.create_role(name=roles[-x].name,permissions=roles[-x].permissions,colour=roles[-x].colour)
+
+
+	async def DeleteRoles(self):
+		urguild = input(f"[{Fore.RED}Cope{Fore.RESET}]~ Your Guild ID{Fore.RED}: {Fore.RESET}")
+		guildOBJ2 = client.get_guild(int(urguild))
+		for a in guildOBJ2.roles:
+			if a.name == guildOBJ2.default_role.name:
+				pass
+			elif a.managed == False: 
+				await a.delete()
+			else:
+				pass
+
+	def Message(self):
+		print(f'''
+[{Fore.RED}HELP{Fore.RESET}] - Help
+[{Fore.RED}1{Fore.RESET}] - Copy Channels
+[{Fore.RED}2{Fore.RESET}] - Copy Roles
+[{Fore.RED}3{Fore.RESET}] - Delete Channels
+[{Fore.RED}4{Fore.RESET}] - Delete Roles
+[{Fore.RED}C{Fore.RESET}] - Credits''')
+
+	def Message2(self):
+		print(f'''
+[{Fore.RED}Github{Fore.RESET}] - 7BZ''')
+
+	async def Menu(self):
+		os.system(f"""cls & mode 50,20 & title Cope - User: {client.user}""")
+		print(f'''
+{Fore.LIGHTBLACK_EX} ▄█▄    ████▄ █ ▄▄  ▄███▄   
+{Fore.LIGHTBLACK_EX} █▀ ▀▄  █   █ █   █ █▀   ▀  
+{Fore.LIGHTBLACK_EX} █   ▀  █   █ █▀▀▀  ██▄▄    {Fore.RESET} Type {Fore.RED}HELP {Fore.RESET}For Help.
+{Fore.LIGHTBLACK_EX} █▄  ▄▀ ▀████ █     █▄   ▄▀ {Fore.RESET} Made by {Fore.RED}sordo{Fore.RESET}
+{Fore.LIGHTBLACK_EX} ▀███▀         █    ▀███▀   
+{Fore.LIGHTBLACK_EX}                ▀           
+{Fore.RESET}''')
+		outputz = input(f"[{Fore.RED}Cope{Fore.RESET}]~ {Fore.RED}: {Fore.RESET}")
+		if outputz.lower() == "help" or outputz.upper() == "help":
+			self.Message()
+			time.sleep(2)
+			await self.Menu()
+		elif outputz == "1":
+			await self.ChannelCopy()
+			time.sleep(2)
+			await self.Menu()
+		elif outputz == "2":
+			await self.RolesCopy()
+			time.sleep(2)
+			await self.Menu()
+		elif outputz == "3":
+			await self.DeleteChannels()
+			time.sleep(2)
+			await self.Menu()
+		elif outputz == "4":
+			await self.DeleteRoles()
+			time.sleep(2)
+			await self.Menu()
+		elif outputz.lower() == "c" or outputz.upper() == "c":
+			self.Message2()
+			time.sleep(2)
+			await self.Menu()
+		else:
+			time.sleep(2)
+			await self.Menu()
+
+@client.event
+async def on_error(error):
+	with open('errors.txt','a') as f:
+		f.write(f"Error: Improper Guild ID has been passed.\n")
+		f.close()
+	time.sleep(2)
+	await Cope().Menu()
+
+@client.event
+async def on_ready():
+	await Cope().Menu()
+
+try:
+	client.run(token,bot=False)
+except Exception as e:
+	with open('errors.txt','a') as f:
+		f.write(f"Error: {e}\n")
+		f.close()
